6988dc89a91da196811804b40fd614454b56e286
ejunjsh <sjj050121014@163.com>
2017-09-04 23:02:02 +0800

    2017_09_04 23:02:02

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curdir=`dirname $0`
 cd $curdir
-curDate=`date`
-echo $curDate > $curDate
+curDate=`date "+%Y_%m_%d %H:%M:%S"`
+echo $curDate >> record
 git add .
 git commit -m "$curDate"
 git push
